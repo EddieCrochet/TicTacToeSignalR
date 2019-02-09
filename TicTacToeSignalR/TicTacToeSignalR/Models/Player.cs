6a57d5cdffb51f71d4bd21e2c69197569f259605
Eddie Crochet <eddiecrochet1994@gmail.com>
2019-02-09 12:00:03 -0600

    Added and began the player class

---
+++ TicTacToeSignalR/TicTacToeSignalR/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TicTacToeSignalR.Models
+{
+    internal class Player
+    {
+        //gets or sets player name. Set at time user registers.
+        public string Name { get; set; }
+
+        //Gets or sets the opponent player. Set when player clicks find opponent in UI.
+        public Player Opponent { get; set; }
+    }
+}
